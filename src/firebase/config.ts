b30a13236ca366387da1804e35485c0a3691fcae
Yash Deore <deoreyash300@gmail.com>
2026-02-10 16:54:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6250117255-5af1e",
+  "appId": "1:277750347122:web:85b725a49b983bf06999eb",
+  "apiKey": "AIzaSyC9LADHZappVgcMVbcB4HK319GV7de-GpY",
+  "authDomain": "studio-6250117255-5af1e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "277750347122"
+};
